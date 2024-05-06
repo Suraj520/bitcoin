627f703e0824517a8e0f90ed0e955591bc26f66e
Suraj <suraj.suraj.4.4.1996@gmail.com>
2024-05-06 20:30:56 +0530

    Update tx_check.cpp

--- src/consensus/tx_check.cpp
+++ src/consensus/tx_check.cpp
@@ -1,60 +1,27 @@
-// Copyright (c) 2017-2021 The Bitcoin Core developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// tx_check.cpp
 
-#include <consensus/tx_check.h>
-
-#include <consensus/amount.h>
-#include <primitives/transaction.h>
-#include <consensus/validation.h>
-
-bool CheckTransaction(const CTransaction& tx, TxValidationState& state)
+// Distribute rewards for evidence transactions
+void DistributeRewards(const CTransaction& tx)
 {
-    // Basic checks that don't depend on any context
-    if (tx.vin.empty())
-        return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-vin-empty");
-    if (tx.vout.empty())
-        return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-vout-empty");
-    // Size limits (this doesn't take the witness into account, as that hasn't been checked for malleability)
-    if (::GetSerializeSize(TX_NO_WITNESS(tx)) * WITNESS_SCALE_FACTOR > MAX_BLOCK_WEIGHT) {
-        return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-oversize");
-    }
+    if (tx.IsEvidenceTransaction()) {
+        // Calculate reward based on the impact of reported incident
+        int64_t reward = CalculateReward(tx);
 
-    // Check for negative or overflow output values (see CVE-2010-5139)
-    CAmount nValueOut = 0;
-    for (const auto& txout : tx.vout)
-    {
-        if (txout.nValue < 0)
-            return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-vout-negative");
-        if (txout.nValue > MAX_MONEY)
-            return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-vout-toolarge");
-        nValueOut += txout.nValue;
-        if (!MoneyRange(nValueOut))
-            return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-txouttotal-toolarge");
+        // Add reward to the uploader's address
+        AddRewardToUploader(tx, reward);
     }
+}
 
-    // Check for duplicate inputs (see CVE-2018-17144)
-    // While Consensus::CheckTxInputs does check if all inputs of a tx are available, and UpdateCoins marks all inputs
-    // of a tx as spent, it does not check if the tx has duplicate inputs.
-    // Failure to run this check will result in either a crash or an inflation bug, depending on the implementation of
-    // the underlying coins database.
-    std::set<COutPoint> vInOutPoints;
-    for (const auto& txin : tx.vin) {
-        if (!vInOutPoints.insert(txin.prevout).second)
-            return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-inputs-duplicate");
-    }
+// Calculate reward for evidence transaction based on impact
+int64_t CalculateReward(const CTransaction& tx)
+{
+    // Placeholder logic to calculate reward based on impact
+    return INITIAL_TOKEN_REWARD - (blockHeight / TOKEN_REWARD_REDUCTION_INTERVAL) * TOKEN_REWARD_REDUCTION_RATE;
+}
 
-    if (tx.IsCoinBase())
-    {
-        if (tx.vin[0].scriptSig.size() < 2 || tx.vin[0].scriptSig.size() > 100)
-            return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-cb-length");
-    }
-    else
-    {
-        for (const auto& txin : tx.vin)
-            if (txin.prevout.IsNull())
-                return state.Invalid(TxValidationResult::TX_CONSENSUS, "bad-txns-prevout-null");
-    }
-
-    return true;
+// Add reward to the uploader's address
+void AddRewardToUploader(const CTransaction& tx, int64_t reward)
+{
+    // Placeholder logic to add reward to uploader's address
+    // You may need to interact with wallet or address management code here
 }
